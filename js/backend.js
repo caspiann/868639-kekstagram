fb9383a65dc5660b9bb72d7e27045f5aef9a8c2d
Andrii Hodniuk <andrii.hodniuk@outlook.com>
2018-10-08 08:18:57 +0200

    Refactored backend module

--- js/backend.js
+++ js/backend.js
@@ -1,16 +1,21 @@
 'use strict';
 
 (function () {
-  window.load = function (url, onSuccess, onError) {
-    var xhr = new XMLHttpRequest();
+  var URL_GET_DATA = 'https://js.dump.academy/kekstagram/data';
+  var URL_SEND_DATA = 'https://js.dump.academy/kekstagram';
+  var TIMEOUT_TIME = 10000;
+  var STATUS_OK = 200;
 
+  var makeRequest = function (onLoad, onError) {
+
+    var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
 
     xhr.addEventListener('load', function () {
-      if (xhr.status === 200) {
-        onSuccess(xhr.response);
+      if (xhr.status === STATUS_OK) {
+        onLoad(xhr.response);
       } else {
-        onError('Status: ' + xhr.status + ' ' + xhr.statusText);
+        onError('Error! Request status: ' + xhr.status + ' ' + xhr.statusText);
       }
     });
 
@@ -22,9 +27,25 @@
       onError('The request failed to complete in ' + xhr.timeout + 'ms');
     });
 
-    xhr.timeout = 10000;
+    xhr.timeout = TIMEOUT_TIME;
+    return xhr;
+  };
 
-    xhr.open('GET', url);
+  var getData = function (onLoad, onError) {
+    var xhr = makeRequest(onLoad, onError);
+    xhr.open('GET', URL_GET_DATA);
     xhr.send();
   };
+
+  var sendData = function (data, onSuccess, onError) {
+    var xhr = makeRequest(onSuccess, onError);
+    xhr.open('POST', URL_SEND_DATA);
+    xhr.send(data);
+  };
+
+  window.backend = {
+    getData: getData,
+    sendData: sendData
+  };
+
 })();
