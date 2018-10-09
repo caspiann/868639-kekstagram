237ee59d299a81f834f0b9c0e609c22bf37e1f92
Andrii Hodniuk <andrii.hodniuk@outlook.com>
2018-10-09 19:34:51 +0200

    Added filter module for pictures

---
+++ js/pictures-filter.js
@@ -0,0 +1,35 @@
+'use strict';
+
+(function () {
+
+  var NEW_PHOTO_MIN = 0;
+  var NEW_PHOTO_MAX = 10;
+
+  var generateNumber = function (min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+  };
+
+  window.newData = {
+    getNewPictures: function () {
+      var newPictures = window.cachePictures.slice();
+      var resultArr = [];
+      for (var i = NEW_PHOTO_MIN; i < NEW_PHOTO_MAX; i++) {
+        var element = newPictures[generateNumber(NEW_PHOTO_MIN, NEW_PHOTO_MAX)];
+        if (resultArr.indexOf(element) !== -1) {
+          element = newPictures[generateNumber(NEW_PHOTO_MIN, NEW_PHOTO_MAX)];
+          i--;
+        } else {
+          resultArr.push(element);
+        }
+      }
+      return resultArr;
+    },
+    getMostDiscussedElements: function () {
+      var newPictures = window.cachePictures.slice();
+      return newPictures.sort(function (first, second) {
+        return second.comments.length - first.comments.length;
+      });
+    }
+  };
+
+})();
